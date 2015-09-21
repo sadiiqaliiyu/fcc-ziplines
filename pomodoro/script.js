c717827ab92bb7a06abeb20d2c2b0057b8d45b06
AlexWulkan <AlexWulkan@users.noreply.github.com>
2015-09-21 22:13:58 +0200

    Added user stats and battery percentage. Deleted jQuery UI and reset button. Did some general fixes.

--- pomodoro/script.js
+++ pomodoro/script.js
@@ -5,32 +5,75 @@ $(document).ready(function () {
   "use strict";
   
   var $mainButton = $('#mainButton'),
-    $span = $('span'),
-    $fillDiv = $('#battery div'),
+    $filler = $('#filler'),
+    $timerText = $('#battery div'),
+    $totalWork = $('div #totalWork'),
+    $totalBreak = $('div #totalBreak'),
     buttonValue = true,
     timerInterval,
     batteryMax = 3600,
-    chargeTimer = 0;
+    batteryMid = batteryMax / 2,
+    batteryLow = batteryMax / 8,
+    chargeTimer = 0,
+    minuteCounter = 0,
+    minuteStorage = 0,
+    breakCounter = 0,
+    breakStorage = 0,
+    batteryPercentage = (batteryMax / 100),
+    timerPercentage;
   
-  function countUp() {
-    chargeTimer += 1;
-    $span.css('transform', 'scaleY(' + chargeTimer / batteryMax + ')');
-    $fillDiv.html(chargeTimer);
-  }
-  
-  function countDown() {
-    chargeTimer -= 3;
-    $span.css('transform', 'scaleY(' + chargeTimer / batteryMax + ')');
-    $fillDiv.html(chargeTimer);
+  // Handles the timer counting up and down, plus converting to percentage.
+  function countUpDown() {
+    if (buttonValue === false) {
+      chargeTimer += 1;
+      
+      // Resets every 60 seconds and stores +1 into a variable.
+      // Also prints to the screen.
+      minuteCounter += 1;
+      if (minuteCounter === 60) {
+        minuteStorage += 1;
+        $totalWork.html(minuteStorage);
+        minuteCounter = 0;
+      }
+    } else {
+      chargeTimer -= 3;
+      
+      // Resets every 60 seconds and stores +1 into a variable.
+      // Also prints to the screen.
+      breakCounter += 1;
+      if (breakCounter === 2) {
+        breakStorage += 1;
+        $totalBreak.html(breakStorage);
+        breakCounter = 0;
+      }
+    }
+    $filler.css('transform', 'scaleY(' + chargeTimer / batteryMax + ')');
+    timerPercentage = (chargeTimer / batteryPercentage).toFixed(1);
+    $timerText.html(timerPercentage + '%');
   }
   
   /*
-  // Emergency stop
-  $('#breakButton').click(function () {
+  // Resets the battery charge and changes button to Charge mode.
+  $('#resetButton').click(function () {
     clearInterval(timerInterval);
-    $('button').prop('disabled', false);
+    chargeTimer = 0;
+    $filler.css('transform', 'scaleY(0)');
+    $timerText.html('0.0%');
+    buttonValue = true;
+    $mainButton.removeClass('btn-danger').addClass('btn-primary').text('Start Working');
   });
   */
+  
+  // Changes the charge color (red, orange, green)
+  function colorAdjuster() {
+    if (chargeTimer < batteryLow) {
+      $filler.css('background-color', '#cc1800');
+    } else if (chargeTimer < batteryMid) {
+      $filler.css('background-color', '#d18b00');
+    } else {
+      $filler.css('background-color', '#58b036');
+    }
+  }
   
   
   // This function reverses the battery fill and
@@ -43,15 +86,16 @@ $(document).ready(function () {
     // If buttonValue is True.
     case true:
       buttonValue = false;
-      $mainButton.removeClass('btn-primary').addClass('btn-danger').text('Discharge');
+      $mainButton.removeClass('btn-primary').addClass('btn-danger').text('Take a Break');
       
       // Start new interval
       timerInterval = setInterval(function () {
         if (chargeTimer < batteryMax) {
-          countUp();
+          countUpDown();
         } else if (chargeTimer >= batteryMax) {
           clearInterval(timerInterval);
         }
+        colorAdjuster();
       }, 1000);
         
       break;
@@ -59,21 +103,28 @@ $(document).ready(function () {
     // If buttonValue is False.
     case false:
       buttonValue = true;
-      $mainButton.removeClass('btn-danger').addClass('btn-primary').text('Charge');
-        
+      $mainButton.removeClass('btn-danger').addClass('btn-primary').text('Start Working');
+      
+      // Start new interval
       timerInterval = setInterval(function () {
         if (chargeTimer >= 3) {
-          countDown();
+          countUpDown();
         } else if (chargeTimer < 3) {
+          // Resets chargeTimer value battery fill.
+          // It also stops the timer and changes display text.
+          chargeTimer = 0;
+          $timerText.html('0.0%');
+          $filler.css('transform', 'scaleY(0)');
           clearInterval(timerInterval);
         }
+        colorAdjuster();
       }, 1000);
         
       break;
     }
   }
   
-  // This is the main Charge/Discharge button.
+  // This is the main Work/Break button.
   $mainButton.click(function () {
     buttonSwitch();
   });

583e3520771cca6294b01eb135b2fe3c54681f4a
AlexWulkan <AlexWulkan@users.noreply.github.com>
2015-09-22 22:02:43 +0200

    Big update with sound warnings, time adjusters, and more.

--- pomodoro/script.js
+++ pomodoro/script.js
@@ -6,21 +6,24 @@ $(document).ready(function () {
   
   var $mainButton = $('#mainButton'),
     $filler = $('#filler'),
-    $timerText = $('#battery div'),
+    $timerText = $('#timerText'),
     $totalWork = $('div #totalWork'),
     $totalBreak = $('div #totalBreak'),
+    $batteryWarning = $('#batteryWarning'),
     buttonValue = true,
     timerInterval,
     batteryMax = 3600,
     batteryMid = batteryMax / 2,
-    batteryLow = batteryMax / 8,
+    batteryLow = batteryMax / 5,
     chargeTimer = 0,
     minuteCounter = 0,
     minuteStorage = 0,
     breakCounter = 0,
     breakStorage = 0,
+    secondsDecreased = 4,
     batteryPercentage = (batteryMax / 100),
-    timerPercentage;
+    timerPercentage,
+    batterySound = false;
   
   // Handles the timer counting up and down, plus converting to percentage.
   function countUpDown() {
@@ -41,28 +44,22 @@ $(document).ready(function () {
       // Resets every 60 seconds and stores +1 into a variable.
       // Also prints to the screen.
       breakCounter += 1;
-      if (breakCounter === 2) {
+      if (breakCounter === 60) {
         breakStorage += 1;
         $totalBreak.html(breakStorage);
         breakCounter = 0;
       }
+      
+      if (chargeTimer < batteryLow && batterySound) {
+        document.getElementById('lowBattery').play();
+        batterySound = false;
+      }
     }
     $filler.css('transform', 'scaleY(' + chargeTimer / batteryMax + ')');
     timerPercentage = (chargeTimer / batteryPercentage).toFixed(1);
+    
     $timerText.html(timerPercentage + '%');
   }
-  
-  /*
-  // Resets the battery charge and changes button to Charge mode.
-  $('#resetButton').click(function () {
-    clearInterval(timerInterval);
-    chargeTimer = 0;
-    $filler.css('transform', 'scaleY(0)');
-    $timerText.html('0.0%');
-    buttonValue = true;
-    $mainButton.removeClass('btn-danger').addClass('btn-primary').text('Start Working');
-  });
-  */
   
   // Changes the charge color (red, orange, green)
   function colorAdjuster() {
@@ -74,7 +71,6 @@ $(document).ready(function () {
       $filler.css('background-color', '#58b036');
     }
   }
-  
   
   // This function reverses the battery fill and
   // Changes the button color/text.
@@ -107,9 +103,9 @@ $(document).ready(function () {
       
       // Start new interval
       timerInterval = setInterval(function () {
-        if (chargeTimer >= 3) {
+        if (chargeTimer >= secondsDecreased) {
           countUpDown();
-        } else if (chargeTimer < 3) {
+        } else if (chargeTimer < secondsDecreased) {
           // Resets chargeTimer value battery fill.
           // It also stops the timer and changes display text.
           chargeTimer = 0;
@@ -124,8 +120,44 @@ $(document).ready(function () {
     }
   }
   
+  function batteryUpdater(batteryMax) {
+    batteryPercentage = (batteryMax / 100);
+    batteryMid = batteryMax / 2;
+    batteryLow = batteryMax / 5;
+  }
+  
+  //Makes sure that the battery is calculating the percentage
+  // anf fill color correctly after changing batteryMax value.
+  function changeBatteryTime(value) {
+    if (value && batteryMax > 600) {
+      batteryMax -= 300;
+      batteryUpdater(batteryMax);
+    } else if (!value) {
+      batteryMax += 300;
+      batteryUpdater(batteryMax);
+    }
+    
+    clearInterval(timerInterval);
+    chargeTimer = 0;
+    $filler.css('transform', 'scaleY(0)');
+    $timerText.html('0.0%');
+    $mainButton.removeClass('btn-danger').addClass('btn-primary').text('Start Working');
+    buttonValue = true;
+    $('#displayMinutes').html(batteryMax / 60);
+    $('#displayBreak').html(Math.ceil((batteryMax / 60) / secondsDecreased));
+  }
+  
+  $('#subtractMinutes').click(function () {
+    changeBatteryTime(true);
+  });
+  
+  $('#addMinutes').click(function () {
+    changeBatteryTime(false);
+  });
+  
   // This is the main Work/Break button.
   $mainButton.click(function () {
     buttonSwitch();
+    batterySound = true;
   });
 });
